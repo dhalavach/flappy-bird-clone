35dec163f1bdf6c59b0e8c653cdbbce66b3235f7
Dzmitry Halavach <kotik1@protonmail.com>
2022-09-11 15:50:36 +0300

    edit script.js

--- script.js
+++ script.js
@@ -7,8 +7,38 @@ function update(time) {
 
 function handleStart() {
   title.classList.add("hide");
-
-  window.requestAnimationFrame(update);
+  setupBird();
+  setupPipes();
+  lastTime = null;
+  window.requestAnimationFrame(updateLoop);
 }
 
-function handleLose() {}
+window.requestAnimationFrame(update);
+function updateLoop(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(updateLoop);
+    return;
+  }
+  const delta = time - lastTime;
+  updateBird(delta);
+  updatePipes(delta);
+  if (checkLose()) return handleLose();
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
+}
+function isCollided(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
+}
+
+function handleLose() {
+  setTimeout(() => {
+    title.classList.remove("hide");
+    document.addEventListener("keypress", handleStart, { once: true });
+  }, 100);
+}
